7a7be3e15965f9104b04550c81cfcc777c423e74
MinhPQ20505 <minhpq2402203@gmail.com>
2025-02-15 19:46:42 +0700

    auth

---
+++ back-end/utils/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized', status: 401 });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.id;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Invalid token', status: 401 });
+  }
+};
+
+module.exports = authMiddleware;
